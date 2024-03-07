7b012383baf2001e078d2fbd71175ba13c2371f0
Maneshr57 <162626367+Maneshr57@users.noreply.github.com>
2024-03-08 00:38:18 +0530

    Add files via upload

---
+++ day1/FireFox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class FireFox {
+ public static void main(String[] args) {
+	
+	float browserVersion = 100.2f;
+	String browserName = "firefox";
+	boolean isVisible = true;
+	int releaseYear = 1998;
+	char browserlogo = 'f';
+	
+	System.out.println("broserVersion is:"+browserVersion);
+	System.out.println("brosername is:"+browserName);
+	System.out.println("isVisible is:"+isVisible);
+	System.out.println("releaseYear is:"+releaseYear);
+	System.out.println("browserlogo is:"+browserlogo);
+	
+			
+}
+}
